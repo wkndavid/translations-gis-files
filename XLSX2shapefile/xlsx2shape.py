1fe38fec4fb0485eba69640458a00b5a2d75a390
wkndavid <workingnown@gmail.com>
2024-07-29 15:10:52 -0300

    [update]

--- XLSX2shapefile/xlsx2shape.py
+++ XLSX2shapefile/xlsx2shape.py
@@ -2,7 +2,7 @@ import geopandas as gpd
 from shapely.geometry import Point
 import pandas as pd
 # Caminho para o arquivo Excel (.xls .xlsx) local
-caminho_excel = '/var/www/today/translations-esri-arcgis-files/'
+caminho_excel = '/var/www/today/translations-esri-arcgis-files/XLSX2shapefile/xlsx-testSHP.xlsx'
 
 # Ler dados do Excel para um DataFrame do pandas
 dados_excel = pd.read_excel(caminho_excel)
@@ -14,7 +14,7 @@ geometry = [Point(0, 0) for _ in range(len(dados_excel))]
 gdf = gpd.GeoDataFrame(dados_excel, geometry=geometry, crs='EPSG:4326')
 
 # Caminho para salvar o shapefile localmente (com a extensão .shp)
-caminho_shapefile = '/var/www/today/translations-esri-arcgis-files/results/result_xlsx2shape.shp'
+caminho_shapefile = '/var/www/today/translations-esri-arcgis-files/XLSX2shapefile/results/result_xlsx2shape.shp'
 
 # SaivarOGeoDataFramee como shapefile usando o GeoPandas
 gdf.to_file(caminho_shapefile, driver='ESRI Shapefile')
