4cd3987fed3d3729d232b20d090915466d06c706
wkndavid <workingnown@gmail.com>
2024-07-30 16:30:34 -0300

    main script DBF2csv

--- DBF2csv/dbf2csv-utf8.py
+++ DBF2csv/dbf2csv-utf8.py
@@ -1,14 +1,23 @@
 import pandas as pd
+from dbfread import DBF
 
 def converter_dbf_para_csv(caminho_dbf, caminho_csv, encoding='latin-1'):
     try:
-        df = pd.read_csv(caminho_dbf, encoding=encoding)
+        # Ler o arquivo DBF
+        dbf = DBF(caminho_dbf, encoding=encoding)
+        
+        # Converter para um DataFrame do pandas
+        df = pd.DataFrame(iter(dbf))
+        
+        # Salvar o DataFrame como um arquivo CSV
         df.to_csv(caminho_csv, index=False, encoding='utf-8')
         print(f"Arquivo convertido para CSV com sucesso: {caminho_csv}")
     except Exception as e:
         print(f"Erro ao converter o arquivo: {e}")
 
-# Use este script para converter seu arquivo .dbf
-caminho_dbf = '/var/www/today/translations-esri-arcgis-files/DBFcsv/dbf2shape.dbf'
-caminho_csv = '/var/www/today/translations-esri-arcgis-files/DBFcsv/dbf-xlsx2shape.csv'
+# Caminho dos arquivos
+caminho_dbf = '/var/www/today/translations-gis-files/DBF2csv/lotes_registrados.dbf'
+caminho_csv = '/var/www/today/translations-gis-files/DBF2csv/deubom.csv'
+
+# Converter o arquivo DBF para CSV
 converter_dbf_para_csv(caminho_dbf, caminho_csv)
--- DBFtranslation/dbf2xlsx.py
+++ DBFtranslation/dbf2xlsx.py
@@ -5,15 +5,14 @@ import logging
 import json
 from art import tprint
 
-#configurando o logging
+# Configurando o logging
 logging.basicConfig(filename='traducao.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-#função para carregar o dicionário de traduções de um arquivo JSON
+# Função para carregar o dicionário de traduções de um arquivo JSON
 def carregar_dicionario(caminho_json):
     try:
         with open(caminho_json, 'r', encoding='utf-8') as f:
-            traducoes = json.load(f)
-        return traducoes
+            return json.load(f)
     except json.JSONDecodeError as e:
         logging.error(f"Erro de decodificação JSON: {str(e)}")
         raise
@@ -24,12 +23,11 @@ def carregar_dicionario(caminho_json):
         logging.error(f"Erro ao carregar o dicionário de traduções: {str(e)}")
         raise
 
-#função para pré-processar o dicionário e armazenar os acrônimos
+# Função para pré-processar o dicionário e armazenar os acrônimos
 def preprocessar_dicionario(traducoes):
-    acronimos = {palavra.upper() for palavra in traducoes.keys()}
-    return acronimos
+    return {palavra.upper() for palavra in traducoes.keys()}
 
-#função para traduzir as células
+# Função para traduzir as células
 def traduzir_celulas(registro, acronimos, traducoes):
     novo_registro = {}
     for coluna, valor in registro.items():
@@ -37,56 +35,46 @@ def traduzir_celulas(registro, acronimos, traducoes):
             novo_registro[coluna] = ''
             continue
 
-        novo_valor = []
         palavras = re.findall(r'\b\w+\b', str(valor))
-        for palavra in palavras:
-            palavra_upper = palavra.upper()
-            if palavra_upper in acronimos:
-                traduzida = traducoes.get(palavra_upper, palavra)
-                novo_valor.append(traduzida)
-            else:
-                novo_valor.append(palavra)
-        novo_registro[coluna] = ' '.join(novo_valor)
+        novo_registro[coluna] = ' '.join(
+            traducoes.get(palavra.upper(), palavra) if palavra.upper() in acronimos else palavra
+            for palavra in palavras
+        )
     return novo_registro
 
-#função para ler um arquivo DBF, traduzir e salvar
+# Função para ler um arquivo DBF, traduzir e salvar
 def traduzir_dbf_e_salvar(dbf_path, output_path, acronimos, traducoes):
     try:
-        tprint("Início do Processamento", font="block")  #título principal usando fonte "block"
+        tprint("Início do Processamento", font="thinkertoy")  # Título principal usando uma fonte mais minimalista
         table = DBF(dbf_path, encoding='latin1')
 
-        registros_traduzidos = []
-        total_registros = len(table)
-        for i, registro in enumerate(table):
-            registros_traduzidos.append(traduzir_celulas(registro, acronimos, traducoes))
-            if (i + 1) % 10 == 0 or i == total_registros - 1:
-                print(f"Traduzindo registros: {i + 1}/{total_registros}")
+        registros_traduzidos = [traduzir_celulas(registro, acronimos, traducoes) for registro in table]
 
         print("Convertendo para DataFrame...")
         df = pd.DataFrame(registros_traduzidos)
 
         print("Salvando em Excel...")
-        df.to_excel(output_path, index=False, engine='openpyxl', encoding='latin1')
+        df.to_excel(output_path, index=False, engine='openpyxl')  # Remover 'encoding'
 
         logging.info("Arquivo DBF traduzido e salvo com sucesso")
-        tprint("Processo Concluído", font="block")  #título principal usando fonte "block"
+        tprint("Processo Concluido", font="thinkertoy")  # Título principal usando uma fonte mais minimalista
     except Exception as e:
         logging.error(f"Erro ao processar o arquivo DBF: {str(e)}")
         print(f"Erro ao processar o arquivo DBF: {str(e)}")
         raise
 
-#caminho para o arquivo JSON com o dicionário de traduções (minificado)
-caminho_json = '/var/www/today/translations-esri-arcgis-files/DBFtranslation/traducao_minify.json'
+# Caminho para o arquivo JSON com o dicionário de traduções (minificado)
+caminho_json = '/var/www/today/translations-gis-files/DBFtranslation/traducao_minify.json'
 
-#carrega o dicionário de traduções do arquivo JSON
+# Carrega o dicionário de traduções do arquivo JSON
 traducoes = carregar_dicionario(caminho_json)
 
-#caminho para o arquivo DBF de entrada
-caminho_dbf = '/var/www/today/translations-esri-arcgis-files/DBFtranslation/dbf-xlsx2shape.dbf'
+# Caminho para o arquivo DBF de entrada
+caminho_dbf = '/var/www/today/translations-gis-files/DBFtranslation/dbf-xlsx2shape.dbf'
 
-#caminho de saída para o arquivo Excel
-caminho_output_dbf = '/var/www/today/translations-esri-arcgis-files/DBFtranslation/results/01today.xlsx'
+# Caminho de saída para o arquivo Excel
+caminho_output_dbf = '/var/www/today/translations-gis-files/DBFtranslation/results/brabo01.xlsx'
 
-#execute as funções para traduzir o DBF e salvar como Excel
+# Executa as funções para traduzir o DBF e salvar como Excel
 acr_preprocessados = preprocessar_dicionario(traducoes)
 traduzir_dbf_e_salvar(caminho_dbf, caminho_output_dbf, acr_preprocessados, traducoes)
